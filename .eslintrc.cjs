1519be08b879abd264e875b72f862e5325ba8dfa
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-04-23 14:31:08 +0200

    update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,5 @@
 module.exports = {
-	extends: [
-		"plugin:react/recommended",
-		"plugin:react-hooks/recommended",
-		"plugin:@typescript-eslint/recommended",
-		"plugin:jsx-a11y/recommended",
-		"plugin:prettier/recommended",
-		"prettier/react",
-		"next/core-web-vitals"
-	],
+	extends: ["next/core-web-vitals"],
 	parser: "@typescript-eslint/parser",
 	plugins: ["@typescript-eslint"],
 	root: true
