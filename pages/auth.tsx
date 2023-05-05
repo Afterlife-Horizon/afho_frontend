3c62cf310e01e1d1591366e7c187977c49e5d71a
nicobonnardot3 <nicobonnardot3@gmail.com>
2023-05-05 16:31:36 +0200

    change auth page to use custom button

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,22 +1,26 @@
-import { Auth } from "@supabase/auth-ui-react"
 import { supabase } from "../utils/supabaseUtils"
 import type { NextPage } from "next"
+import { Button } from "@/components/ui/button"
+import { FaDiscord } from "react-icons/fa"
 
 const AuthPage: NextPage = () => {
+	async function handleLogin() {
+		await supabase.auth.signInWithOAuth({
+			provider: "discord",
+			options: {
+				scopes: "identify",
+				redirectTo: process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URI || "http://localhost:3000"
+			}
+		})
+	}
 	return (
 		<div className="discord-login">
-			<Auth
-				supabaseClient={supabase}
-				onlyThirdPartyProviders={true}
-				redirectTo={process.env.NEXT_PUBLIC_SUPABASE_REDIRECT_URI}
-				appearance={{
-					className: {
-						button: "blue-button",
-						loader: "spinner"
-					}
-				}}
-				providers={["discord"]}
-			/>
+			<div>
+				<Button className="blue-button" onClick={async () => await handleLogin()}>
+					<FaDiscord size={"1.5rem"} className="mr-2" />
+					Login with Discord
+				</Button>
+			</div>
 		</div>
 	)
 }
